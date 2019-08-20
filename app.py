6d8a6fd0c0c35555f75cb8fbe0c2db2a5a312513
xiusl <xiushilin@hotmail.com>
2019-08-20 17:34:00 +0800

    fix

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask, request
 from .weibo import WeiboSpider
 
 app = Flask(__name__)
+app.config['PORT'] = 5001
 app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
 app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 
@@ -16,14 +17,20 @@ celery.conf.update(app.config)
 @celery.task
 def getWeibo(url):
     data = sp.getWeiboByUrl(url)
+    return data
 
+@celery.task(bind=True)
+def getWeiboo(self, url):
+    sp = WeiboSpider()
+    d = sp.getWeiboByUrl(url)
+    return d
 
 @app.route('/weibo', methods=['POST'])
 def weibo():
     data = request.get_json()
     url = data.get('url')
-    getWeibo.delay(url)
-    return {'ok': 1}
+    res = getWeibo.delay(url)
+    return {'id': str(res.id)}
 
 if __name__ == '__main__':
     app.run(port=5001)
